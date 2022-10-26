1c1c21048650b6b722d21edfc8522e57df1126cd
St4rkes <moisesfragahbl@gmail.com>
2022-10-26 19:55:22 -0300

    A

--- src/AgendaBD/Main.java
+++ src/AgendaBD/Main.java
@@ -1,93 +1,96 @@
 package AgendaBD;
 
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args)throws Exception {
-        Scanner input = new Scanner(System.in);
-        //criando um objeto para manipular os objetos no BDA
-       AgendaDAO dao = new AgendaDAO();
-        
-        boolean rodar = true;
-        while(rodar) {
-
+        try (Scanner teclado = new Scanner(System.in)) {
+            //criando um objeto para manipular os objetos no BDA
+      AgendaDAO dao = new AgendaDAO();
             
-        System.out.println("\n\nSistema de cadastro de pessoa");
-        System.out.println("1--- Mostra lista de pessoas");
-        System.out.println("2--- Buscar por pessoa");
-        System.out.println("3--- Cadastrar Pessoa");
-        System.out.println("4--- Alterar pessoa");
-        System.out.println("5--- Remover pessoa da lista");
-        System.out.println("6--- Sair");
-        System.out.println("Digite a opcao desejada");
+            boolean rodar = true;
+            while(rodar) {
 
-        int opcao = input.nextInt();
-
-        switch(opcao){
-            case 1:
-                ArrayList<Pessoa> listar = dao.Listar();
-              
-            break;
-
-            case 2:
-                System.out.println("Digite o ID");
-                int pessoaId = input.nextInt();
-                Pessoa p = dao.obterPessoa(pessoaId);
-                System.out.println(p);
-            break;
-
-            case 3:
-                System.out.println("CADASTRO DE PESSOA:");
-                System.out.println("Digite o seu id");
-                int id = input.nextInt();
                 
-                System.out.println("Digite o seu nome");
-                String nome = input.nextLine();
-                
+            System.out.println("\n\nSistema de cadastro de pessoa");
+            System.out.println("1--- Mostra lista de pessoas");
+            System.out.println("2--- Buscar por pessoa");
+            System.out.println("3--- Cadastrar Pessoa");
+            System.out.println("4--- Alterar pessoa");
+            System.out.println("5--- Remover pessoa da lista");
+            System.out.println("6--- Sair");
+            System.out.println("Digite a opcao desejada");
 
-                System.out.println("Seu telefone");
-                String telefone = input.nextLine();
+            int opcao = teclado.nextInt();
 
-                System.out.println("Sua Data de nascimento");
-                String data = input.nextLine();
-
-                Pessoa pessoa = new Pessoa(id, nome, telefone, data);
-                dao.inserir(pessoa);
+            switch(opcao){
+                case 1:
+                    ArrayList<Pessoa> listar = dao.Listar();
+                    for(Pessoa p: listar){
+                        System.out.println(p);
+                    }
+                  
                 break;
 
-            // case 4:
-            //     System.out.println("Digite o id da Pessoa desejada realizar a alteracao");
-            //     int idPessoa = input.nextInt();
-            //     Pessoa pessoaAlterar = dao.getPessoaPorId(idPessoa);
-            //     System.out.println(pessoaAlterar);
-            //     System.out.println("Novo nome");
-            //     pessoaAlterar.nome = input.nextLine();
-            //     pessoaAlterar.nome = input.nextLine();
-            //     System.out.println("novo telefone");
-            //     pessoaAlterar.telefone = input.nextLine();
-            //     System.out.println("nova data de nascimento");
-            //     pessoaAlterar.data = input.nextLine();
-            //     dao.alterar(pessoaAlterar);
-            // break;
+                case 2:
+                    System.out.println("Digite o ID");
+                    int pessoaId = teclado.nextInt();
+                    Pessoa p = dao.obterPessoa(pessoaId);
+                    System.out.println(p);
+                break;
 
-            case 5:
-             System.out.println("Digite o i da pessoa desejada remover");
-                int delete = input.nextInt();
-                dao.remover(dao.obterPessoa(delete));
-            break;
+                case 3:
+                    System.out.println("CADASTRO DE PESSOA:");
+                    System.out.println("Digite o seu id");
+                    int id = teclado.nextInt();
+                    
+                    System.out.println("Digite o seu nome");
+                    String nome = teclado.nextLine();
+                    
 
-            case 6: 
-                System.out.println("Volte sempre !");
-                rodar = false;
-            break;
+                    System.out.println("Seu telefone");
+                    String telefone = teclado.nextLine();
 
-            default:
-            System.out.println("Tente novamente !");
+                    System.out.println("Sua Data de nascimento");
+                    String data = teclado.nextLine();
 
+                    Pessoa pessoa = new Pessoa(id, nome, telefone, data);
+                    dao.inserir(pessoa);
+                    break;
+
+                // case 4:
+                //     System.out.println("Digite o id da Pessoa desejada realizar a alteracao");
+                //     int idPessoa = input.nextInt();
+                //     Pessoa pessoaAlterar = dao.getPessoaPorId(idPessoa);
+                //     System.out.println(pessoaAlterar);
+                //     System.out.println("Novo nome");
+                //     pessoaAlterar.nome = input.nextLine();
+                //     pessoaAlterar.nome = input.nextLine();
+                //     System.out.println("novo telefone");
+                //     pessoaAlterar.telefone = input.nextLine();
+                //     System.out.println("nova data de nascimento");
+                //     pessoaAlterar.data = input.nextLine();
+                //     dao.alterar(pessoaAlterar);
+                // break;
+
+                case 5:
+                 System.out.println("Digite o i da pessoa desejada remover");
+                    int delete = teclado.nextInt();
+                    dao.remover(dao.obterPessoa(delete));
+                break;
+
+                case 6: 
+                    System.out.println("Volte sempre !");
+                    rodar = false;
+                break;
+
+                default:
+                System.out.println("Tente novamente !");
+
+            }
+     }
         }
-      }
 	
     }
 }
